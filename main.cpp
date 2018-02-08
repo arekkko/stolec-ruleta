5a1156ac290efe1681d6d025d2e4630b122d3397
arekkko <36229331+arekkko@users.noreply.github.com>
2018-02-08 21:13:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <cstring>
 #include <ctime>
 
-#include "sprawdz.cpp"
+//#include "sprawdz.cpp"
 
 using namespace std;
 
@@ -20,13 +20,15 @@ int StanGracz(int KontoGracza);
 int Czysc();
 void Zasady();
 int Losowanie();
+void sprawdzKoniec();
+
+int Pojedyncza(int pojedyncza, int losowana, int Pula);
+
+
+
 
 int main(){
 	int n;
-
-
-
-
 	do{
 		Menu();
 			cin >> n;
@@ -63,7 +65,7 @@ int main(){
 
 void Zasady(){
 	cout << "1. Numery parzyste, albo nieparzyste - wygrana = k * 1" << endl;
-	cout << "2. Dowolny jeden numer, za wyj¹tkiem 0, wygrana = k*35" << endl;
+	cout << "2. Dowolny jeden numer, za wyj1tkiem 0, wygrana = k*35" << endl;
 	cout << "3. Dwa kolejne numery, wygrana = k * 17" << endl;
 	cout << "4. Trzy kolejne numery, wygrana = k * 11" << endl;
 	cout << "WYNIK RZUTU 0 ZAWSZE OZNACZA WYGRANA KASYNA!!!" << endl;
@@ -93,7 +95,7 @@ void OProgramie(){
 
 void Menu(){
 	cout << "Wybierz liczbe!" << endl;
-	cout << "1. Wybierz liczbe" << endl;
+	cout << "1. GRAJ" << endl;
 	cout << "2. Zasady!" << endl;
 	cout << "3. Stan konta gracza" << endl;
 	cout << "4. Stan konta kasyna" << endl;
@@ -102,20 +104,23 @@ void Menu(){
 }
 
 int Losowanie(){
-	srand(time(NULL));
 
-	int losowana;
+srand(time(NULL));
 
-	losowana = rand()%38;
+int losowana;
 
-	int Zaklad;
-	cout << "Obstaw liczbe!!!" << endl;
-	cin >> Zaklad;
+losowana = rand()%38;
+
+	//int Zaklad;
+//	cout << "Obstaw liczbe!!!" << endl;
+//	cin >> Zaklad;
 	int Pula;
 	cout << "Podaj ile pieniedzy obstawiasz!!!" << endl;
 	cin >> Pula;
-	int Wygrana;
-	cout << "Wylosowales: " << losowana << endl;
+
+	cout<<"\nAktualnie obstawiasz: "<<Pula<<" zetonow\n"<<endl;
+//	int Wygrana;
+/*	cout << "Wylosowales: " << losowana << endl;
 
 	if(losowana == Zaklad){
 		cout << "Brawo, to ta sama liczba!!!" << endl;
@@ -125,8 +130,102 @@ int Losowanie(){
 		cout << "Nastepnym razem Ci sie uda!!!" << endl;
 	}
 
-	sprawdz();
+	sprawdz(); */
+
+
+			cout << "1 - Obstaw pojedyncza liczbe" << endl;
+			cout << "2 - Obstaw parzysta lub nieparzysta" << endl;
+			cout << "3 - Obstaw tuziny" << endl;
+			cout << "4 - Obstaw 3 liczby obok siebie" << endl;
+			cout << "5 - Obstaw 2 liczby obok siebie" << endl;
+			cout << "0 - Powrot / zmiana wysokosci zakladu" << endl;
+
+	int n;
+	int pojedyncza;
+	int parzysta;
+	int tuzin;
+	int liczba1, liczba2, liczba3;
+	int liczba4, liczba5;
+
+	do{
+		cin >> n;
+
+		switch(n){
+			case 1: do{
+                        cout << "Wprowadz pojedyncza liczbe" << endl;
+                        cin >> pojedyncza;
+                    }while(pojedyncza<1 || pojedyncza>37);
+					Pojedyncza(pojedyncza, losowana, Pula);
+                    sprawdzKoniec();
+					break;
+			case 2:
+                    cout << "Wprowadz parzysta lub nieparzysta" << endl;
+					cin >> parzysta;
+					break;
+			case 3:
+                    cout << "Wprowadz jeden z tuzinow" << endl;
+					cin >> tuzin;
+					break;
+			case 4:
+                    cout << "Wprowadz 3 liczby obok siebie" << endl;
+					cin >> liczba1;
+					cin >> liczba2;
+					cin >> liczba3;
+					break;
+			case 5:
+                    cout << "Wprowadz 2 liczby obok siebie" << endl;
+					cin >> liczba4;
+					cin >> liczba5;
+					break;
+			default: cout << "Podaj jeszcze raz" << endl;
+					break;
+		}
+	}while(n!=0);
+
     return 0;
 }
+
+
+
+int Pojedyncza(int Pojedyncza, int losowana, int Pula){
+
+	int WygranaPojedyncza;
+
+	cout<<"Wylosowana liczba to: "<<losowana<<endl;
+
+	WygranaPojedyncza = Pula*35;
+
+	if (losowana==0){
+        cout << "Przegrałeś czlowieku" << endl;
+	}else{
+	    if(Pojedyncza == losowana){
+            cout << "Wygrales czlowieku" << endl;
+            KontoGracza += WygranaPojedyncza;
+            KontoKasyna -= WygranaPojedyncza;
+        }else{
+            cout << "Przegrales czlowieku" << endl;
+            KontoGracza -= WygranaPojedyncza;
+            KontoKasyna += WygranaPojedyncza;
+        }
+	}
+
+
+	cout<<"\nTwoj aktualny stan konta:"<<KontoGracza<<endl;
+
+	return 0;
+}
+
+void sprawdzKoniec(){
+
+    if(KontoGracza<=0){
+        cout<<"Twoj stan konta: "<<KontoGracza<<" PRZEGRALES| KONIEC GRY"<<endl;
+    }
+    if(KontoKasyna<=0){
+        cout<<"Twoj stan konta: "<<KontoGracza<<" WYGRALES | Stan konta kasyna: "<<KontoKasyna<<endl;
+    }
+
+}
+
+
 
 
